1366386be533e0a745955fae767c417d953caf00
Shakiba448 <shakiba448@gmail.com>
2021-05-02 15:11:20 +0600

    WIP on premium webstie

--- src/sections/testimonial.js
+++ src/sections/testimonial.js
@@ -121,6 +121,7 @@ export default function TestimonialCard() {
                     <Heading as="h4" sx={styles.heading}>
                       {item.name}
                     </Heading>
+                    <Text sx={styles.designation}>{item.designation}</Text>
                   </div>
                 </div>
               </Box>
